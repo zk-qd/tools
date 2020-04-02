cfa4aedca221512029b5d2b73df275fcc140102c
zk-qd <2605066922@qq.com>
2020-04-02 20:29:50 +0800

    newly content

--- Array.js
+++ Array.js
@@ -1,5 +1,12 @@
-var ArrayKit = {
+var ArrayKit_Context = {
+    dateSort/* 输入小时对象 然后进行排序 */(data) {
+        return Object.keys(data).sort().map(item => data[item]);
+    },
+}
 
+
+var ArrayKit = {
+    ...ArrayKit_Context,
 }
 
 // 数组去重
