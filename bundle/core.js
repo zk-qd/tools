3952524585dd06a5d3fbfd0cb64ee4bae592cbdb
zk-qd <2605066922@qq.com>
2020-04-03 16:23:06 +0800

    merge commit

--- bundle/core.js
+++ bundle/core.js
@@ -1,15 +1,27 @@
 //Array.js
-var ArrayKit = {
 
+var ArrayKit_Context = {
+    dateSort(data) {
+        return Object.keys(data).sort().map(item => data[item]);
+    },
 }
+
+
+var ArrayKit = {
+    ...ArrayKit_Context,
+}
+
+
+
 //Date.js
 
 
 var DateKit = {
-    dateFormat:/* data format */ function (date, format) {
+    dateFormat:function (date, format) {
         if (!date) date = new Date();
         else date = new Date(date);
         if (!format) format = 'yyyy-MM-dd';
+       
         const list = [
             { match: 'yyyy', val: date.getFullYear() },
             { match: 'MM', val: (date.getMonth() + 1 + '').padStart(2, '0') },
@@ -25,11 +37,12 @@ var DateKit = {
         }
         return format;
     }
-}
+}
 //Form.js
-
+// operation
 var FormKit_Operation = {
-    getForm({ formSel, prefix } = /* 依赖error */ErrorKit.emptyParameterException()) {
+   
+    getForm({ formSel, prefix } =ErrorKit.emptyParameterException()) {
         var form = document.querySelector(formSel);
         if (!form) return FormKit_Message['form-notExist'];
         var data = form.serializeArray();
@@ -46,9 +59,12 @@ var FormKit_Operation = {
         }
         return data;
     },
-    setForm({ formSel, prefix, params } = /* 依赖error */ErrorKit.emptyParameterException()) {
+   
+    setForm({ formSel, prefix, params } =ErrorKit.emptyParameterException()) {
+       
         const form = document.querySelector(formSel);
         if (!form) return FormKit_Message['form-notExist'];
+       
         let ele;
         for (var key in params) {
             if (prefix) ele = form.querySelector('[name=' + prefix + key + ']');
@@ -57,9 +73,10 @@ var FormKit_Operation = {
         }
     }
 }
+
 var FormKit_verify = {
 
-}
+}
 var FormKit_Message = {
     ['form-notExist']: 'The form doesn\'t exist',
 }
@@ -75,9 +92,9 @@ var FormKit = {
 
 var HttpKit_Judge = {
 
-}
+}
 var HttpKit_Uri = {
-    query2string/* query转字符串 */(...params) {
+    query2string(...params) {
         params = params.reduce((total, item) => ({ ...total, ...item }), {})
         var querystring = '';
         if (params) {
@@ -85,30 +102,35 @@ var HttpKit_Uri = {
                 querystring += key + '=' + params[key];
                 querystring += '&';
             }
+           
             if (querystring.match(/&$/g)) querystring = querystring.slice(0, querystring.length - 1);
             return querystring;
         }
     },
-    uri3query/* 合并uri以及query */(uri, ...params) {
+    uri3query(uri, ...params) {
         var querystring = this.query2string(...params);
+       
         uri.indexOf('?') == -1 ? uri += '?' + querystring : uri += querystring;
         return uri;
     },
-    clear1void/* 清除无效的属性 undefined null 空字符串不在内 */(obj) {
+   
+    clear1void(obj) {
         for (let key in obj) {
             let value = obj[key]
             if (value == undefined) delete obj[key];
         }
         return obj;
     },
-    clear1allVoid/* 清除全部无效的属性 undefined null 空字符串包含在内 */(obj) {
+   
+    clear1allVoid(obj) {
         for (let key in obj) {
             let value = obj[key]
             if (value == undefined || value === '') delete obj[key];
         }
         return obj;
     },
-    clear1assignVoid/* 清除指定的属性 */(obj, assign = [], filter = 'clear1allVoid') {
+   
+    clear1assignVoid(obj, assign = [], filter = 'clear1allVoid') {
         obj = this[filter](obj);
         for (let key in obj) {
             var value = obj[key];
@@ -126,19 +148,19 @@ var HttpKit = {
 }
 
 //Judge.js
-
+
 var JudgeKit_Judge = {
-    void2empty/* 转换undefined以及null */(value) {
+    void2empty(value) {
         if (value == undefined) return '';
         else return value;
     },
-    allVoid2empty/* 转换undefined以及null并 'undefined' 和 'null'*/(value) {
+    allVoid2empty(value) {
         if (value == undefined) return '';
         else if (value === 'undefined') return '';
         else if (value === 'null') return '';
         else return value;
     },
-    allVoid2bar/* 没有意义的字符串以及null undefined转成横条 */(value) {
+    allVoid2bar(value) {
         var value = this.allVoid2empty(value)
         if (value === '') return '-';
         else return value;
@@ -167,11 +189,13 @@ var ObjectKit = {
                 o[i] = this.sCopy(obj[i]);
             }
         } else {
+           
             o = obj.valueOf();
         }
         return o;
     }    
 }
+
 
 
 
