53362ac45d941d52b2c1443b347bafb4fd079d73
zk-qd <2605066922@qq.com>
2020-04-03 14:23:03 +0800

    merge commit

--- core/Array.js
+++ core/Array.js
@@ -1,4 +1,10 @@
 
+var ArrayKit_Context = {
+    dateSort/* 输入小时对象 然后进行排序 */(data) {
+        return Object.keys(data).sort().map(item => data[item]);
+    },
+}
+
 
 var ArrayKit = {
     ...ArrayKit_Context,
